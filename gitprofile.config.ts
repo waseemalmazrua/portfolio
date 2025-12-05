9d3bbd4674da62464c4645065555b476299f191e
Waseem Almazrua <gakalmoot12@gmail.com>
2025-12-05 16:31:50 +0300

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -2,7 +2,7 @@
 
 const CONFIG = {
   github: {
-    username: 'waseemalmazrua', // Your GitHub org/user name. (This is the only required config)
+    username: 'waseemalmazrua', // Your GitHub org/user name. (This is the only required config) waseem
   },
   /**
    * If you are deploying to https://<USERNAME>.github.io/, for example your repository is at https://github.com/arifszn/arifszn.github.io, set base to '/'.
